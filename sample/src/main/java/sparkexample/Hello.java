144c77681c738aca19a1fbd9db49cf03374cc045
gone-phishing <riteshoneinamillion@gmail.com>
2015-05-31 21:57:05 +0530

    Moving the test project to sample folder

---
+++ sample/src/main/java/sparkexample/Hello.java
@@ -0,0 +1,10 @@
+package sparkexample;
+
+import static spark.Spark.get;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("This is working");
+    }
+}
