ec14da4ce768e6628979e717987174766cb14b7f
gone-phishing <riteshoneinamillion@gmail.com>
2015-08-19 02:18:20 +0530

    Successfully logging into single ec2 instance

--- aws/spark_aws.java
+++ aws/spark_aws.java
@@ -1,6 +1,9 @@
+package sample;
+
 import java.io.*;
 import java.util.logging.Logger;
 import com.jcraft.jsch.*;
+//import com.jcraft.jsch.JSchException;
 
 /**
  * Usage :
@@ -10,20 +13,97 @@ import com.jcraft.jsch.*;
  * args[3] = "privateKeyFile"
  */
 
-class spark_aws
+public class spark_aws
 {
 	private static final Logger log = Logger.getLogger(spark_aws.class.getName());
 	private String privateKeyFile="";
 	private String username="";
 	private String hostname="";
+	private Session session;
 
-	spark_aws(String username, String hostname, String privateKeyFile)
+	public spark_aws(String username, String hostname, String privateKeyFile)
 	{
 		System.out.println("Single node setup");
 		this.username = username;
 		this.hostname = hostname;
 		this.privateKeyFile = privateKeyFile;
 		run_single_instance();
+	}
+
+	public void run_single_instance()
+	{
+		try
+		{
+			Session session = getSession();
+			session.connect();
+			execute_sudo(session, "pwd;", "");
+			session.disconnect();
+		}
+		catch(JSchException je)
+		{
+			je.printStackTrace();
+		}
+	}
+
+	public Session execute_sudo(Session session, String command, String passwd)
+	{
+		try
+		{
+			ChannelExec channel=(ChannelExec) session.openChannel("exec");
+	      	//channel.setCommand("sudo -S -p '' "+command);
+			channel.setCommand(command);
+			//BufferedReader reader = new BufferedReader(new InputStreamReader(channel.getInputStream()));
+			InputStream in=channel.getInputStream();
+	      	OutputStream out=channel.getOutputStream();
+	      	channel.setErrStream(System.err);
+
+	      	channel.connect();
+
+	      	out.write((passwd+"\n").getBytes());
+	      	out.flush();
+
+	      	byte[] tmp=new byte[1024];
+	      	while(true)
+	      	{
+	      	  	while(in.available()>0)
+	      	  	{
+	      	    	int i=in.read(tmp, 0, 1024);
+	      	    	if(i<0)break;
+	      	    	System.out.print(new String(tmp, 0, i));
+	      	  	}
+	      	  	if(channel.isClosed())
+	      	  	{
+	      	  		if(channel.getExitStatus() == 0)
+	      	  		{
+	      	  			System.out.println("Execution successful");
+	      	  		}
+	      	  		else
+	      	  		{
+	      	  			System.out.println("exit-status: "+channel.getExitStatus());
+	      	  		}
+	      	    	break;
+	        	}
+	        	try
+	        	{
+	        		Thread.sleep(1000);
+	        	}
+	        	catch(Exception ee)
+	        	{
+	        		ee.printStackTrace();
+	        	}
+	      	}
+			channel.disconnect();
+	    }
+	    catch(JSchException jex)
+	    {
+	      	jex.printStackTrace();
+	    }
+	    catch(IOException iex)
+	    {
+	    	iex.printStackTrace();
+	    }
+
+	    return session;
 	}
 
 	private Session getSession() throws JSchException
@@ -42,19 +122,21 @@ class spark_aws
 
 	public static void main(String[] args)
 	{
-		if(args[0].equals("single"))
+		if(args.length >= 4)
 		{
-			System.out.println("Single node setup");
-			spark_aws(args[1],args[2]);
-		}
-		else if(args[0].equals("cluster"))
-		{
-			System.out.println("Initiate cluster setup");
+			if(args[0].equals("single"))
+			{
+				new spark_aws(args[1], args[2], args[3]);
+			}
+			else if(args[0].equals("cluster"))
+			{
+				System.out.println("Initiate cluster setup");
+			}
 		}
 		else
 		{
-			System.out.println("Correct format of execution is: \n<setup-type> : \"single\" or \"cluster\" ");
+			System.out.println("Correct format of execution is: \n<setup-type> : [\"single\"] | [\"cluster\"] <username> <hostname> <privateKeyFile>");
 		}
-		new spark_aws();
+		System.out.println("That's all folks !!");
 	}
 }
